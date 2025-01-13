76e3b809659e83931846abaca98cc4ce86d9563b
Tien Nguyen <tnguyen0393@gmail.com>
2025-01-12 22:01:14 -0600

    fixed callback bug and configured title and description

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import cardDeck from "../data/cards.json";
 import Card from "./components/Card";
 import styles from "./page.module.css";
